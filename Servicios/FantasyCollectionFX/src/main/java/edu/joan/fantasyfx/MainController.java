0033c713bacb552814b30dd5c22987626f0b9daf
Joan Pomares <joanpomaresherrero@gmail.com>
2025-10-28 20:57:39 +0100

    ui made

---
+++ Servicios/FantasyCollectionFX/src/main/java/edu/joan/fantasyfx/MainController.java
@@ -0,0 +1,11 @@
+package edu.joan.fantasyfx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
+public class MainController {
+    @FXML TableView<String> tv = new TableView<String>();
+
+    
+}

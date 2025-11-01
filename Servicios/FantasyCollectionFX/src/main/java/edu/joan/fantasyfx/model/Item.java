701746d87f4ea0addc6cf2c6b9d4413ddcd32d25
JP27 <joanpomaresherrero@gmail.com>
2025-11-01 11:17:13 +0100

    some javadocs that wasn't implemented

--- Servicios/FantasyCollectionFX/src/main/java/edu/joan/fantasyfx/model/Item.java
+++ Servicios/FantasyCollectionFX/src/main/java/edu/joan/fantasyfx/model/Item.java
@@ -16,6 +16,14 @@ public class Item implements Serializable {
         this.code = code;
     }
 
+    /**
+     * creates an item with the passed parameters
+     * @param code the code of the item
+     * @param name the name of the item
+     * @param type the type of the item
+     * @param rarity the rarity of the item
+     * @param date the obtained date of the item
+     */
     public Item(String code, String name, String type, String rarity, LocalDate date) {
         this.code = code;
         this.name = name;
@@ -24,43 +32,83 @@ public class Item implements Serializable {
         this.date = date;
     }
 
+    /**
+     * Gives the code of the item
+     * @return a string representing the code of the item
+     */
     public String getCode() {
         return code;
     }
 
+    /**
+     * sets the code of the item
+     * @param code code to set
+     */
     public void setCode(String code) {
         this.code = code;
     }
 
+    /**
+     * gets the name of the item
+     * @return String representing the name of the item
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * sets the name of the item
+     * @param name name to be set
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * gets the type of the item
+     * @return String representing the type of the item
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * sets the type of the item
+     * @param type type to be set
+     */
     public void setType(String type) {
         this.type = type;
     }
 
+    /**
+     * gets the rarity of the item
+     * @return String representing the rarity of the item
+     */
     public String getRarity() {
         return rarity;
     }
 
+    /**
+     * Sets the rarity of the item
+     * @param rarity rarity to set
+     */
     public void setRarity(String rarity) {
         this.rarity = rarity;
     }
 
+    /**
+     * Gets the obtained date in a formatted string
+     * @return String representing the obtained date of the item
+     */
     public String getDate() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         return formatter.format(date);
     }
 
+    /**
+     * Sets the date of the item
+     * @param date the LocalDate to be set
+     */
     public void setDate(LocalDate date) {
         this.date = date;
     }
